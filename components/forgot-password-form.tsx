6e2ae645c63193730881dde37b4cb9f635e5e5c0
gywns2zang9 <gywns2zang9@naver.com>
2025-12-11 19:13:47 +0900

    feat - 방명록 좋아요/싫어요 기능 추가

--- components/forgot-password-form.tsx
+++ components/forgot-password-form.tsx
@@ -76,7 +76,7 @@ export function ForgotPasswordForm({
                   <Input
                     id="email"
                     type="email"
-                    placeholder="m@example.com"
+                    placeholder="abc123@example.com"
                     required
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
